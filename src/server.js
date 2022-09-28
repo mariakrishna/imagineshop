10282a8c81c7c90f54f9797cbb70d69697c29d58
mariakrishna <mkadonai@gmail.com>
2022-09-27 22:56:43 -0300

    heroku config

--- src/server.js
+++ src/server.js
@@ -80,6 +80,6 @@ app.put("/users/:id", async (req, res) => {
   return res.status(404).json({ message: "Usuário não encontrado." });
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
 });
